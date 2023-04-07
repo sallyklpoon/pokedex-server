61e9a3ca15ecf4ec57efe45b1c40106875b8948f
Sally Poon <sallypoon@hotmail.com>
2023-04-07 15:16:59 -0700

    Fix auth User and auth Admin

--- controllers/server.js
+++ controllers/server.js
@@ -6,15 +6,15 @@ const { asyncWrapper } = require('../helpers/asyncWrapper.js');
 const User = require('../models/user');
 const Token = require('../models/token');
 const Pokemon = require('../models/pokemon');
+const Request = require('../models/request.js');
 const { parseAccessToken } = require('../helpers/parseTokens');
-const { setAccessToken } = require('../helpers/cookie.js');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const REFRESH_TOKEN_HEADER = 'auth-token-refresh';
 const ACCESS_TOKEN_HEADER = 'auth-token-access';
-const ACCESS_EXPIRY = '20';
+const ACCESS_EXPIRY = '20s';
 
 const {
     PokemonAuthError,
@@ -24,41 +24,44 @@ const {
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: '*',
     exposedHeaders: [REFRESH_TOKEN_HEADER, ACCESS_TOKEN_HEADER],
 }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  });
 
 const hashedPassword = async password => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 };
 
-const authUser = asyncWrapper(async (req, res, next) => {
-    const token = req.header('authorization');
-    if (!token) {
+const authUser = async accessToken => {
+    if (!accessToken) {
         throw new PokemonAuthError("No Token: Please provide the access token using the headers.");
     }
-
-    const accessToken = await parseAccessToken(token);
     try {
-        const verified = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
-        next();
+        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('User has access. :)');
+            }
+        })
     } catch (err) {
         throw new PokemonAuthError("Invalid Token Verification. Log in again.");
     }
-});
+};
 
-const authAdmin = asyncWrapper(async (req, res, next) => {
-    const token = req.header('authorization');
-    const accessToken = await parseAccessToken(token);
-
+const authAdmin = async accessToken => {
     const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
     console.log(payload);
     if (payload?.user?.role == "admin") {
         return next();
     }
     throw new PokemonAuthError("Access denied");
-});
+};
 
 app.post('/register', asyncWrapper(async (req, res) => {
     const { username, password, email, role } = req.body;
@@ -69,7 +72,6 @@ app.post('/register', asyncWrapper(async (req, res) => {
         const user = await User.create(newUser);
         res.send(user);
     } catch (err) {
-        console.log(err);
         if (err.code == 11000) {
             throw new PokemonDbError('Email or Username is already in use. Please try again.');
         } else {
@@ -80,16 +82,16 @@ app.post('/register', asyncWrapper(async (req, res) => {
 
 app.post('/login', asyncWrapper(async (req, res) => {
     const { username, password } = req.body;
-    if (!username || !password) throw new PokemonAuthError("Incorrect Payload");
+    if (!username || !password) res.status(401).json('Login Failure');
 
     const user = await User.findOne({ username });
 
     if (!user) throw new PokemonAuthError("User not found");
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) throw new PokemonAuthError("Password is incorrect");
+    if (!isPasswordCorrect) res.status(401).json('Login Failure');
 
-    const accessToken = jwt.sign({ user: user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_EXPIRY });
+    const accessToken = jwt.sign({ user: user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
     const refreshToken = jwt.sign({ user: user }, process.env.REFRESH_TOKEN_SECRET);
 
     Token.create({ token: refreshToken, valid: true });
@@ -102,7 +104,6 @@ app.post('/login', asyncWrapper(async (req, res) => {
 app.get('/logout', asyncWrapper(async (req, res) => {
     try {
         const refreshToken = req.headers[REFRESH_TOKEN_HEADER];
-        console.log(refreshToken);
 
         let invalidateToken = await Token.findOneAndDelete({ token: refreshToken });
         if (invalidateToken) {
@@ -118,13 +119,12 @@ app.get('/logout', asyncWrapper(async (req, res) => {
     }
 }));
 
-app.post('/requestNewAccessToken', asyncWrapper(async (req, res) => {
-    const token = req.header.REFRESH_TOKEN_HEADER;
+app.get('/requestNewAccessToken', asyncWrapper(async (req, res) => {
+    const refreshToken = req.headers[REFRESH_TOKEN_HEADER];
 
-    if (!token) {
+    if (!refreshToken) {
         throw new PokemonAuthError("No token: Please provide a token.");
     }
-    const refreshToken = await parseRefreshToken(token);
     if (!refreshToken) {
         throw new PokemonAuthError("Invalid Token: Refresh token not found. Please provide a valid token.");
     }
@@ -135,20 +135,22 @@ app.post('/requestNewAccessToken', asyncWrapper(async (req, res) => {
     }
     try {
         const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-        const newAccessToken = jwt.sign({ user: payload.uesr }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_EXPIRY });
+        const newAccessToken = jwt.sign({ user: payload.uesr }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
         res.header(ACCESS_TOKEN_HEADER, newAccessToken);
-        res.send("All good!");
+        res.send("New access token, sent!");
     } catch (e) {
         throw new PokemonAuthError("Invalid Token: Please provide a valid token.");
     };
 }));
 
 
-// app.use(authUser);
 /**
  * Get all pokemons based on filters of count and after.
  */
 app.get('/pokemons', async (req, res) => {
+    let accessToken = req.headers['auth-token-access'];
+    await authUser(accessToken);
+
     try {
         const pokemonUrl = 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json';
         const pokemonRes = await fetch(pokemonUrl);
@@ -159,7 +161,20 @@ app.get('/pokemons', async (req, res) => {
     }
 });
 
+/**
+ * Get a Pokemon based on id.
+ */
+app.get('/pokemon/:id', async (req, res) => {
+    try {
+        // proof of concept! IRL, it's more efficient to pull from all Pokemons data stored in local storage.
+        return res.status(200).json('Pika pika!');
+    } catch (err) {
+        return res.status(400).json({ errMsg: 'Error fetching pokemon.' });
+    }
+});
+
 app.get('/pokemonTypes', async (req, res) => {
+    authUser(req.headers['auth-token-access'])
     try {
         const typesUrl = 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/types.json';
         const typesRes = await fetch(typesUrl);
@@ -169,6 +184,17 @@ app.get('/pokemonTypes', async (req, res) => {
         throw new PokemonDbError('Error retrieving types. Please try again.')
     }
 });
+
+app.post('/request/create', async (req, res) => {
+    const requestData = req.body;
+
+    try {
+        const reqRes = await Request.create(requestData);
+        res.status(200).send(reqRes);
+    } catch (err) {
+        res.status(501).send('Pokemon DB error: error tracking endpoint request.')
+    }
+})
 
 
 app.use((_req, res) => {
--- models/request.js
+++ models/request.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const requestSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        default: 'unauthorized'
     },
     endpoint: {
         type: String,
